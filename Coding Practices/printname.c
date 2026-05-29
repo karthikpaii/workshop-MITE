38f9078025d8624acb897134cd26c2042509156d
Karthik <thekarthikpai@gmail.com>
2026-05-29 20:44:59 +0530

    Update printname.c

--- Coding Practices/printname.c
+++ Coding Practices/printname.c
@@ -7,3 +7,19 @@ void main(){
     scanf("%s",&college);
     printf("College is: %s \n",college);
 }
+
+//version 2
+
+
+#include<stdio.h>
+
+int main(){
+    char str[50];
+    
+    printf("Enter a String:");
+    
+    if(fgets(str,sizeof(str), stdin)!=NULL){
+        printf("You Entered: %s",str);
+    }else
+    printf("error");
+}
